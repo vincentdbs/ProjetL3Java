4901299cbd00a4ff9148bb2f002396725ba59dc4
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-05-08 09:49:27 +0200

    ajout class RC

---
+++ src/com/projet/TypeQuestion/RC.java
@@ -0,0 +1,26 @@
+package com.projet.TypeQuestion;
+
+public class RC {
+    private String texte, bonneReponse;
+
+    public RC(String texte, String bonneReponse) {
+        this.texte = texte;
+        this.bonneReponse = bonneReponse;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
+
+    public String getBonneReponse() {
+        return bonneReponse;
+    }
+
+    public void setBonneReponse(String bonneReponse) {
+        this.bonneReponse = bonneReponse;
+    }
+}

1c6e9bc0a3653f179c59937ba543f93b4e1e6f2b
vincent_dubois_efrei_net <vincent.dubois@efrei.net>
2020-06-07 11:38:37 +0200

    suppression code commenté inutile [Tools]

--- src/com/projet/Main.java
+++ src/com/projet/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         //todo supprimer toute les méthodes et attributs non utilisés
-        //todo verifier que tout les switch d'introspection du type de question analyse les bons noms de packages
+        //todo verifier que tout les switch d'introspection du type de question analyse les bons noms de packages (sans majuscule après commit ceb4127)
         //lancement du menu principal
         GUI_MenuPrincipal menuPrincipal = new GUI_MenuPrincipal();
     }
--- src/com/projet/Tools.java
+++ src/com/projet/Tools.java
@@ -35,31 +35,28 @@ public final class Tools {
      */
     public static Chronometre getLowestChronometer(Chronometre[] chronometres){
         return Arrays.stream(chronometres)
-                .min(new Comparator<Chronometre>() {
-                    @Override
-                    public int compare(Chronometre o1, Chronometre o2) {
-                        if (o1.getHour() < o2.getHour()) {
+                .min((o1, o2) -> {
+                    if (o1.getHour() < o2.getHour()) {
+                        return -1;
+                    } else if (o1.getHour() > o2.getHour()) {
+                        return 1;
+                    } else {
+                        if (o1.getMinute() < o2.getMinute()) {
                             return -1;
-                        } else if (o1.getHour() > o2.getHour()) {
+                        } else if (o1.getMinute() > o2.getMinute()) {
                             return 1;
                         } else {
-                            if (o1.getMinute() < o2.getMinute()) {
+                            if (o1.getSecond() < o2.getSecond()) {
                                 return -1;
-                            } else if (o1.getMinute() > o2.getMinute()) {
+                            } else if (o1.getSecond() > o2.getSecond()) {
                                 return 1;
                             } else {
-                                if (o1.getSecond() < o2.getSecond()) {
+                                if (o1.getMilisecond() < o2.getMilisecond()) {
                                     return -1;
-                                } else if (o1.getSecond() > o2.getSecond()) {
+                                } else if (o1.getMilisecond() > o2.getMilisecond()) {
                                     return 1;
                                 } else {
-                                    if (o1.getMilisecond() < o2.getMilisecond()) {
-                                        return -1;
-                                    } else if (o1.getMilisecond() > o2.getMilisecond()) {
-                                        return 1;
-                                    } else {
-                                        return 0;
-                                    }
+                                    return 0;
                                 }
                             }
                         }
@@ -73,31 +70,28 @@ public final class Tools {
      */
     public static Chronometre getGreatestChronometer(Chronometre[] chronometres){
         return Arrays.stream(chronometres)
-                .min(new Comparator<Chronometre>() {
-                    @Override
-                    public int compare(Chronometre o1, Chronometre o2) {
-                        if (o1.getHour() < o2.getHour()) {
-                            return 1;
-                        } else if (o1.getHour() > o2.getHour()) {
+                .min((o1, o2) -> {
+                    if (o1.getHour() < o2.getHour()) {
+                        return 1;
+                    } else if (o1.getHour() > o2.getHour()) {
+                        return -1;
+                    } else {
+                        if (o1.getMinute() < o2.getMinute()) {
+                            return 11;
+                        } else if (o1.getMinute() > o2.getMinute()) {
                             return -1;
                         } else {
-                            if (o1.getMinute() < o2.getMinute()) {
-                                return 11;
-                            } else if (o1.getMinute() > o2.getMinute()) {
+                            if (o1.getSecond() < o2.getSecond()) {
+                                return 1;
+                            } else if (o1.getSecond() > o2.getSecond()) {
                                 return -1;
                             } else {
-                                if (o1.getSecond() < o2.getSecond()) {
+                                if (o1.getMilisecond() < o2.getMilisecond()) {
                                     return 1;
-                                } else if (o1.getSecond() > o2.getSecond()) {
+                                } else if (o1.getMilisecond() > o2.getMilisecond()) {
                                     return -1;
                                 } else {
-                                    if (o1.getMilisecond() < o2.getMilisecond()) {
-                                        return 1;
-                                    } else if (o1.getMilisecond() > o2.getMilisecond()) {
-                                        return -1;
-                                    } else {
-                                        return 0;
-                                    }
+                                    return 0;
                                 }
                             }
                         }
@@ -110,23 +104,6 @@ public final class Tools {
      * Renvoie le ou les joueurs ayant le plus petit score parmi le tableau en paramètre
      */
     public static Joueur[] getJoueursLowestScore(Joueur[] joueurs){
-//        ArrayList<Integer> joueursEliminie = new ArrayList<>();
-//        int index = 0;
-//        for (int i = 1; i < joueurs.length ; i++) {
-//            if(joueurs[i].getScore() < joueurs[index].getScore()){
-//                index = i;
-//            }
-//            else if(joueurs[i].getScore() == joueurs[index].getScore()){
-//                if(chrono[i].compareTo(chrono[index]) > 0){
-//                    index = i;
-//                }
-//                else if(chrono[i].compareTo(chrono[index]) == 0){
-//                    joueursEliminie.add(i);
-//                }
-//            }
-//        }
-
-
         //récupération du score le plus bas
         int scoreMin = joueurs[0].getScore();
         for (int i = 1; i < joueurs.length; i++) {
